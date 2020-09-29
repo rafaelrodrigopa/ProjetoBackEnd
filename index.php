d4c3ea946dcab6667b82cf2411d026144943c8f5
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2020-09-28 22:06:52 -0300

    Commit Inicial

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Uni Help Desk</title>
+
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <link rel="shortcut icon" href="./assets/img/support.png" type="image/x-icon">
+
+    <style>
+        .card-login{
+            padding: 30px 0 0 0;
+            width: 350px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+
+    <nav class="navbar navbar-dark bg-dark">
+        <!-- Navbar content -->
+        <a href="" class="navbar-brand">
+            <img src="./assets/img/support.png" alt="" height="32" width="32" class="d-inline-block align-top"> Uni Help Desk
+        </a>
+    </nav>
+    
+    <div class="container">
+        <div class="row">
+            <div class="card-login">
+                <div class="card">
+                    <div class="card-header">
+                        Login
+                    </div>
+                    <div class="card-body">
+                        <form method="POST" action="valida_login.php">
+                            <div class="form-group">
+                                <input type="email" name="email" id="email" class="form-control" placeholder="e-mail">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" name="senha" id="senha" class="form-control" placeholder="senha">
+                            </div>
+                            <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ valida_login.php
@@ -0,0 +1,28 @@
+<?php
+
+    $usuario_autenticado = false;
+    $usuario_id = null;
+    $usuario_perfil_id = null; //Tipo de usuário se for igual a 1 admin senão comum
+
+    $lista_usuarios = array(
+        array('id' => 1, 'email' => 'rafael@teste.com','senha' => '123', 'perfil' => 1),
+        array('id' => 2, 'email' => 'admin2@teste.com','senha' => '123', 'perfil' => 1),
+        array('id' => 3, 'email' => 'comum@teste.com','senha' => '123', 'perfil' => 2),
+        array('id' => 4, 'email' => 'comum2@teste.com','senha' => '123', 'perfil' => 2)
+    );
+
+    foreach($lista_usuarios as $user){
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+            $usuario_autenticado = true;
+            $usuario_id = $user['id'];
+            $usuario_perfil_id = $user['perfil'];
+            break;
+        }
+    }
+
+    if ($usuario_autenticado) {
+        echo "Autenticou";
+    }else{
+        echo "Não autenticou";
+    }
+?>
